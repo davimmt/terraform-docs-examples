f437c3d19a89d3bdfb5aa2003b5965632e6080f2
davimmt <mirandadavi.mt@gmail.com>
2021-10-01 23:33:28 -0300

    close to the end

--- dummy-module/README.md
+++ dummy-module/README.md
@@ -1,18 +1,43 @@
 <!-- BEGIN_TF_EXAMPLES -->
-module "dummy" {
+module "dummy"
   source = ./dummy-module
-  environment = string [__required__]
-  instance_number = number [1]
-  root_block_device = {
-    encrypted   = bool
-    volume_size = number
-    volume_type = string
-  } [{
-    "encrypted"   = true
-    "volume_size" = 10
-    "volume_type" = "gp3"
-  }]
-  security_group_ids = list [[]]
-  tags = map [{}]
+  variable "environment" {
+  description = "Environment type"
+  validation {
+  condition = contains(["Development", "Homologation", "Production"], var.environment)
+  error_message = "Valid values include Development, Homologation, Production."
+  }
+  }
+  variable "instance_number" {
+  default = 1
+  description = "Number of instances"
+  type = number
+  }
+  variable "root_block_device" {
+  default = {
+  "encrypted" = true
+  "volume_size" = 10
+  "volume_type" = "gp3"
+  }
+  description = "Root block devices"
+  type = object({
+  encrypted = bool
+  volume_size = number
+  volume_type = string
+  })
+  validation {
+  condition = contains(["gp2", "gp3", "io1", "io2", "sc1", "st1"], var.root_block_device.volume_type)
+  error_message = "Valid values include standard, gp2, gp3, io1, io2, sc1, or st1. Defaults to gp2."
+  }
+  }
+  variable "security_group_ids" {
+  default = []
+  description = "Secutiry group ids"
+  type = list
+  }
+  variable "tags" {
+  default = {}
+  description = "Resource's Tags"
+  type = map
 }
 <!-- END_TF_EXAMPLES -->
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 from itertools import islice
 
 if __name__ == "__main__":
-    # Getting all *-module directories and its *.tf files
-    # @return => {'./dir-module': ['file.tf', 'file.tf']}
+    """ Getting all *-module directories and its *.tf files
+    
+    @return => {'./dir-module': ['file.tf']}
+    """
     paths = {}
     for dir in [dir[0] for dir in os.walk('.') if dir[0].endswith('-module')]:
         files = [file for file in os.listdir(dir) if file.endswith('.tf')]
@@ -11,9 +13,10 @@ if __name__ == "__main__":
             paths[dir] = []
             paths[dir] += files
 
-    # Grouping all lines from all files in a directory
-    # @return => {'readme': {'metadata': {'module': 'name', 'source': 'path'}},
-    #                       {'data': ['line', 'line']}}
+    """ Grouping all lines from all files in a directory
+     
+    @return => {'readme': {'metadata': {'module': 'name', 'source': 'path'}}, {'data': ['line']}}
+    """
     readmes = {}
     for path in paths:
         module = path.split('/')[-1].split('-module')[0]
@@ -26,23 +29,57 @@ if __name__ == "__main__":
                 while '\n' in line: line = line.replace('\n', '')
                 if line: readmes[readme]['data'].append(line)
 
-    # Remove all lines that don't belong to a variable block
+    """ Tagging all lines that don't belong to a variable block
+    
+    @return => {'readme': {'data': [line_number]}}
+    """
+    lines_to_ignore = {}
     for readme in readmes:
+        lines_to_ignore[readme] = []
         inside_block = False
         block_count = 0
-        for line in readmes[readme]['data']:
-            if line.startswith('variable'): 
-                inside_block = True
+        for i, line in enumerate(readmes[readme]['data']):
+            if line.startswith('variable'): inside_block = True
             if inside_block:
                 if '{' in line or '[' in line: block_count += 1
                 if '}' in line or ']' in line: block_count -= 1
                 if block_count == 0: inside_block = False
-                print('1', line)
-            else: 
-                print(inside_block, block_count, line)
-                readmes[readme]['data'].remove(line)
+            else:
+                lines_to_ignore[readme].append(i)
 
-    exit()
+    """ Grouping separete variable blocks in lists
+    
+    @return => {'readme': {'data': [['block_line']]}}
+    """
+    blocks = {}
     for readme in readmes:
+        blocks[readme] = {}
+        blocks[readme]['data'] = []
+        block = []
         for i, line in enumerate(readmes[readme]['data']):
-            print(line)
+            if i not in lines_to_ignore[readme]:
+                if line.startswith('variable'): 
+                    if len(block) > 0: blocks[readme]['data'].append(block)
+                    block = [line]
+                elif i + 1 == len(readmes[readme]['data']): blocks[readme]['data'].append(block)
+                else: block.append(line)
+    
+    """ Substituting the first lines-based dictionary/list
+        to a blocks-based dictionary/list
+    
+    @return => {'readme': {'metadata': {'module': 'name', 'source': 'path'}}, {'data': [['block_line']}}
+    """ 
+    for readme in readmes:
+        readmes[readme]['data'] = []
+        for block in blocks[readme]['data']:
+            readmes[readme]['data'] += block 
+
+    for readme in readmes:
+        with open(readme, 'w') as file:
+            file.write('<!-- BEGIN_TF_EXAMPLES -->')
+            file.write('\nmodule "%s"' % (readmes[readme]['metadata']['module']))
+            file.write('\n  %s = %s' % ('source', readmes[readme]['metadata']['source']))
+            for line in [block for block in readmes[readme]['data']]:
+                file.write('\n  %s' % (line))
+            file.write('\n}')
+            file.write('\n<!-- END_TF_EXAMPLES -->')
